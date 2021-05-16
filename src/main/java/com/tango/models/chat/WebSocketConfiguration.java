c67e5b05b99b2f16b26099f71bf0596dd06cfc1c
mskKote <msk.vitaly@gmail.com>
2021-05-16 22:38:09 +0300

    chat fix

--- src/main/java/com/tango/models/chat/WebSocketConfiguration.java
+++ src/main/java/com/tango/models/chat/WebSocketConfiguration.java
@@ -2,7 +2,6 @@ package com.tango.models.chat;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,7 +12,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/chatting/connect").withSockJS();
+        registry.addEndpoint("/api/chatting/connect").setAllowedOrigins("*").withSockJS();
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
